fdf0b03cd247a864c7c0381c3f1296541b16c696
Santosh Kumar <msksantosh@evitca.com>
2011-09-12 12:54:46 +0530

    Improved the user show page and added a gravatar

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,6 +18,21 @@ render_views
       get :show, :id => @user
       assigns(:user).should == @user
     end
+
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selectro('title', :content => @user.name)
+    end
+
+    it "should have the user's name" do
+      get :show, :id => @user
+      response.should have_selector('h1', :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_Selector('h1>img', :class => "gravatar")
+    end
   end
 
   describe "GET 'new'" do
