4eb6c0c4aa7bf337fc11afb6fa6ff8ccaf4ad60d
Santosh Kumar <msksantosh@evitca.com>
2011-09-12 18:59:23 +0530

    Added a Sessions controller and signin view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   
-  def new
-    @user = User.new
-    @title = "Sign up"
-  end
-
   def show
     @user = User.find(params[:id])
     @title = @user.name
   end
+
+  def new
+    @user = User.new
+    @title = "Sign up"
+  end  
 
   def create
     @user = User.new(params[:user])
