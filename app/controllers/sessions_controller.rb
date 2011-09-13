89ec21805fb1f4a5b1a514afc2abd7cd5f5cf398
Santosh Kumar <msksantosh@evitca.com>
2011-09-13 13:22:44 +0530

    Marking user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,8 @@ class SessionsController < ApplicationController
     @title = "Sign in"
     render 'new'
   else
+    sign_in user
+    redirect_to user
     # Handle successful sign in.
   end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#
+
+
 class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -22,8 +36,12 @@ class User < ActiveRecord::Base
   class << self
     def authenticate(email, submitted_passsword)
       user = find_by_email
-      return nil if user.nil?
-      return user ifuser.has_password?(submitted_password)
+      (user && user.has_password?(submitted_password)) ? user :nil
+    end
+
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user :nil
     end
   end
 
@@ -46,6 +64,7 @@ class User < ActiveRecord::Base
     Digest::SHA2.hexdigest(string)
   end
 end
+
 
 
 
