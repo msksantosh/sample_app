6606483df8a5f5a7af3651293a35a3ed686e574f
Santosh Kumar <msksantosh@evitca.com>
2011-09-13 15:15:24 +0530

    Completed signin/out

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -100,6 +100,11 @@ response.should render_template('new')
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
       end
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end
 end
 end
