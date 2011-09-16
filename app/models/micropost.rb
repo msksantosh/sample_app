63acff6a117f09e16c5f1327a6ed3d1978925253
Santosh Kumar <msksantosh@evitca.com>
2011-09-16 14:32:13 +0530

    Micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -14,6 +14,8 @@ class Micropost < ActiveRecord::Base
 
   belongs_to :user
 
+  validates :content, :presence => true, :lenght => { :maximum => 140}
+  validates :user_id, :presence => true
   default_scope :order => 'created_at DESC'
 end
 
